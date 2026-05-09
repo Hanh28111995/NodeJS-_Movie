dfb5421fd64cad4d365a95cff5cc282522da4e0d
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2026-05-09 18:35:02 +0700

    edit controller

--- movie-ticket-api/src/controller/customer/ticket.js
+++ movie-ticket-api/src/controller/customer/ticket.js
@@ -19,11 +19,6 @@ export const bookMytickets = asyncHandler(async (req, res) => {
   }
 });
 
-// export const confirmMytickets = asyncHandler(async (req, res) => {
-//   const { ticketId } = req.body;
-//   const ticket = await ticketService.confirmTicket(ticketId);
-//   return sendSuccess(res, "Ticket confirmed successfully", ticket);
-// });
 
 export const cancelMytickets = asyncHandler(async (req, res) => {
   const { ticketId } = req.body;
