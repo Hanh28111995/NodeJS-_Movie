af37b570e24249838f2deed3f664d715673b057f
Hanh28111995 <gm.tranhuuhanh@gmail.com>
2026-08-23 13:44:32 +0700

    redis intergration

--- movie-ticket-api/src/service/ticketService.js
+++ movie-ticket-api/src/service/ticketService.js
@@ -1,4 +1,4 @@
-import redisClient from "../config/redis.js";
+import redisClient from "../config/Redis.js";
 import InforTicket from "../model/inforTicketModel.js";
 import Showtime from "../model/showtimeModel.js";
 import Notification from "../model/userCartNotificationModel.js";
